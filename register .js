b9d2f3035d244b4d9b0e5b10f65ab12d4a45c9df
kittymitt4 <kaydancehamilton10@gmail.com>
2025-04-20 22:18:30 -0400

    crashing out

---
+++ register .js
@@ -0,0 +1,15 @@
+<script type="module">
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyAzj3tP-ogNEQehO9LGjJYmv0vB_z_PtNM",
+    authDomain: "beyonce-bcef6.firebaseapp.com",
+    projectId: "beyonce-bcef6",
+    storageBucket: "beyonce-bcef6.firebasestorage.app",
+    messagingSenderId: "43649939134",
+    appId: "1:43649939134:web:260b20fbeac64c0d1610ef"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
